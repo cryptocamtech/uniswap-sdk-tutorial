4d3cf2c3d7125ba2ea035cc92f19514fe0471ed9
cryptocamtech <camhrich@gmail.com>
2021-06-29 04:44:05 +1000

    put limit back to 500000

--- ethToDai.js
+++ ethToDai.js
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2020, Cameron Hamilton-Rich
+    Copyright (c) 2021, Cameron Hamilton-Rich
 
     Permission to use, copy, modify, and/or distribute this software for any
     purpose with or without fee is hereby granted, provided that the above
@@ -93,7 +93,7 @@ const init = async () => {
         { 
             value: inputAmountHex, 
             gasPrice: gasPrice.toHexString(),
-            gasLimit: ethers.BigNumber.from(1500000).toHexString()
+            gasLimit: ethers.BigNumber.from(500000).toHexString()
         }
     );
 
--- fork_main.sh
+++ fork_main.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 source .env
 
-for (( ; ; ))
-do
+#for (( ; ; ))
+#do
     pkill -f "node /usr/local/bin/ganache-cli"
-    ganache-cli --fork https://mainnet.infura.io/v3/$INFURA_PROJECT_ID -v -i 1 --unlock 0x742d35Cc6634C0532925a3b844Bc454e4438f44e & 
+#    ganache-cli --fork https://mainnet.infura.io/v3/$INFURA_PROJECT_ID -v -i 1 --unlock 0x742d35Cc6634C0532925a3b844Bc454e4438f44e & 
+    ganache-cli --fork http://localhost:8545 -v -i 1 --unlock 0x742d35Cc6634C0532925a3b844Bc454e4438f44e -p 8900 & 
     sleep 2
     node getSomeEth.js;
     # wait for < 30 mins
-    sleep 1750
-done
+#    sleep 1750
+#done
